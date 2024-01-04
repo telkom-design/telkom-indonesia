efc89260bccf8d49f42d22153281e716f49a95d9
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2024-01-05 02:03:49 +0700

    feat: server render

--- src/app/sites/about-telkom/[[...slug]]/page.tsx
+++ src/app/sites/about-telkom/[[...slug]]/page.tsx
@@ -1,50 +1,18 @@
-"use client";
+import { headers } from 'next/headers'
+import React from 'react'
 
-import "../style.scss";
+const AboutTelkom = async() => {
+  try{
+    const header = headers()
+    const {json} = await fetch('https://telkom-co-id-tciops-dev.apps.osh.telkom.co.id'+header.get('x-current-path'))
+    const data = await json()
+    console.log(data)
+    return (
+      <div>{JSON.stringify(data)}</div>
+    )
+  }catch(err){
+    //err handling
+  }
+}
 
-// atoms
-import { Box, Image, Text, Flex } from "@/app/_components/atoms";
-
-// organisms
-import { Newsletter } from "@/app/_components/organisms";
-
-const ProfileAndBriefHistory = ({params}:   {params: {slug: string}}) => {
-  return (
-    <>
-      <Box mt="56px" mb="56px" className="about-us--content">
-        <Box className="about-us--content-wrapper">
-          <Image
-            src="/about-us/58-tahun-telkom.webp"
-            width="100%"
-            height="500px"
-            fit="cover"
-            radius="10px"
-          />
-        </Box>
-
-        <Box className="about-us--content-wrapper">
-          <Text mb="24px" as="h3">
-            58 Journey to Contribute - Telkom Indonesia
-          </Text>
-
-          <Text as="p" color="tertiary500" size="16px" mb="16px">
-            A 58-year journey to contribute to the nation is embodied through
-            consistency in providing diverse digital solutions for the
-            advancement of the country. Telkom Indonesia is committed to making
-            innovations come true and continuously evolving. Thank you for your
-            unwavering trust and support.
-            <br />
-            <br />
-            #TelkomSinergiUntukIndonesia
-            <br />
-            #ElevatingYourFuture
-          </Text>
-        </Box>
-      </Box>
-
-      <Newsletter />
-    </>
-  );
-};
-
-export default ProfileAndBriefHistory;
+export default AboutTelkom
